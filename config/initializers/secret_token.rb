95826e270c58a482d798044fbf775a8d08a4478e
bethanyr <bethany.rentz@hotmail.com>
2013-07-15 09:31:30 -0700

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkblog::Application.config.secret_token = 'b613c28ceaa7e8e69d03e4c63c6247a22b70b5250a2c9d272026b018f0f9864dfdc0e903fec6226a2002cfbe666b3d80acf6df5bc6cb70ad37cd397bebc6cd26'
